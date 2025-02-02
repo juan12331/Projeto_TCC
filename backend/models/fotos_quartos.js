57de30ce61f8ad7150bed908f95c85e14826a7d9
Juan12331 <juanxadez2@gmail.com>
2025-02-02 19:20:46 -0300

    criando tabela fotos_quartos

---
+++ backend/models/fotos_quartos.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize')
+const database = require('../config/sequelize')
+const { default_type } = require('mime')
+const quartos = require('./quartos')
+
+const fotos_quartos = database.define('usuarios', {
+    id_foto: {
+        type: Sequelize.STRING,
+        AllowNUll: false,
+        primaryKey: true,
+        unique: true
+    },
+    imagem: {
+        type: Sequelize.STRING(300),
+        AllowNUll: false
+    },
+    id_quarto: {
+        type: Sequelize.STRING,
+        AllowNUll: false,
+        references: {
+            model: quartos,
+            key: 'id_quarto'
+        },
+    },
+});
+
+quartos.hasMany(fotos_quartos, {
+    foreignKey: 'id_quarto'
+})
+
+fotos_quartos.belongsTo(quartos, {
+    foreignKey: 'id_quarto'
+});
+
+
+module.exports = fotos_quartos
