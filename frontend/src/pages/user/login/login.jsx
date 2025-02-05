b7ac919353a20c45ce519505093f14e690390c6c
Isabela Manzano <44615503890@escola928.edu>
2025-02-05 14:45:31 -0300

    login

---
+++ frontend/src/pages/user/login/login.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+import Login from "./pages/user/login/login.css";
+
+
+function Login() {
+    return (
+      <div className='login'>
+
+        <h1>Login</h1>
+
+        <button>entrar</button>
+
+      </div>
+
+    );
+  }
+  
+  export default Login;
