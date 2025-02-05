5f216b611eb217926deb2d3d50fecc92643a6a2f
juan12331 <juanxadez2@gmail.com>
2025-02-05 08:40:11 -0300

    importando todos os arquivos em routes.js

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,4 +1,15 @@
 const express = require('express')
 const routes = express.Router();
 
+const UsuariosControllers = require('../controllers/usuarios');
+const AvaliacoesControllers = require('../controllers/avaliacoes');
+const Avaliacoes_quartosControllers = require('../controllers/avaliacoes_quartos');
+const Fotos_quartosControllers = require('../controllers/fotos_quartos');
+const ReservasControllers = require('../controllers/reservas')
+
+// função dos usuarios
+
+routes.post('/login', UsuariosControllers.login);
+routes.post('/usuarios', UsuariosControllers.createUsuario);
+
 module.exports = routes
