b1ccd7ec04f6e684a3db689caa5c16cb6dfd93ae
juan12331 <juanxadez2@gmail.com>
2025-02-05 09:42:02 -0300

    criando crud de usuarios

--- backend/controllers/usuarios.js
+++ backend/controllers/usuarios.js
@@ -29,4 +29,46 @@ exports.login = async (req, res) => {
     }
 }
 
+exports.getUsersByCpf = async (req, res) => {
+    try {
+        const encontrarUsuario = await Usuarios.findByPk(req.params.cpf);
+        if (!encontrarUsuario) {
+            return res.status(404).send('Usuario not found');
+        }
+        return res.send(encontrarUsuario);
+    } catch (error) {
+        return res.status(500).send('Internal Server Error');
+    }
+}
+
+
+exports.deleteUsuario = async (req, res) => {
+    const encontrarUsuario = await Usuarios.findOne({ where: { cpf: req.params.cpf } })
+    try {
+        await encontrarUsuario.destroy();
+        return res.send('usuario deletado')
+    } catch (err) {
+        return res.send('aqui deu erro mn se liga', err)
+    }
+}
+
+exports.updateUsuario = async (req, res) => {
+    const Cpf = req.params.cpf
+    console.log(Cpf)
+    const CpfUsuario = await Usuarios.findOne({ where: { cpf: Cpf } })
+    console.log(CpfUsuario)
+
+    if (CpfUsuario) {
+        try {
+            const [Updates] = await Usuarios.update(req.body, { where: { cpf: req.params.cpf } }) // verifica se tem alguma alteração
+            return res.send({ message: 'Usuario foi atualizado ;P', })
+
+        } catch (error) {
+            return res.send('deu erro aqui meu mano ==> ', error)
+
+        }
+    }
+    return res.send('usuario not found!!!')
+}
+
 
