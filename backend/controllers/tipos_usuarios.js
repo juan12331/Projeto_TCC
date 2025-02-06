6847dd0ce1d512ddfaf2d891ea869ec0631b3947
juan12331 <juanxadez2@gmail.com>
2025-02-06 08:56:30 -0300

    criando função de criar tipo de usuario

--- backend/controllers/tipos_usuarios.js
+++ backend/controllers/tipos_usuarios.js
@@ -3,15 +3,29 @@ const TiposUsuarios = require('../models/tipos_usuarios');
 
 exports.createTiposUsuarios = async (req, res) => {
     try {
-        const verificacao = await TiposUsuarios.findByPk(req.params.cpf);
+        console.log('sla')
+        const verificacao = await TiposUsuarios.findByPk(req.params.permissao);
+        console.log('sla')
         if (verificacao) {
+            console.log('sla2')
             return res.send('Tipo de usuario ja foi cadastrado')
         }
-
+        console.log('sla')
         const Tipo = await TiposUsuarios.create(req.body)
+        console.log('sla')
         console.log(Tipo)
+      
+        
         return res.send('tipo de usuario cadastrado com sucesso')
     } catch (err) {
         return res.status(403).send('erro')
+    }
+}
+
+exports.getUsuarios = async (req, res) => {
+    try{
+
+    } catch (err) {
+        return res.status(403).send('error')
     }
 }
