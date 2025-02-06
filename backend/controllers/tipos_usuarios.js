ea30bbb2d582880b620520285ee3911f8e373055
juan12331 <juanxadez2@gmail.com>
2025-02-06 09:23:36 -0300

    terminando tipos_usuarios

--- backend/controllers/tipos_usuarios.js
+++ backend/controllers/tipos_usuarios.js
@@ -1,18 +1,14 @@
-const { Op } = require('sequelize');
+const { Op, ConnectionAcquireTimeoutError } = require('sequelize');
 const TiposUsuarios = require('../models/tipos_usuarios');
+const tipos_usuarios = require('../models/tipos_usuarios');
 
 exports.createTiposUsuarios = async (req, res) => {
     try {
-        console.log('sla')
         const verificacao = await TiposUsuarios.findByPk(req.params.permissao);
-        console.log('sla')
         if (verificacao) {
-            console.log('sla2')
             return res.send('Tipo de usuario ja foi cadastrado')
         }
-        console.log('sla')
         const Tipo = await TiposUsuarios.create(req.body)
-        console.log('sla')
         console.log(Tipo)
       
         
@@ -22,10 +18,38 @@ exports.createTiposUsuarios = async (req, res) => {
     }
 }
 
-exports.getUsuarios = async (req, res) => {
+exports.getTiposUsuarios = async (req, res) => {
     try{
+        
+        const { id_tipo, permissao } = req.query || {};
+
+    if (!id_tipo && !permissao) {
+        const user = await tipos_usuarios.findAll();
+        return res.send(user)
+    }
+
+    const pesquisa = {
+        [Op.or]: [
+            id_tipo ? { id_tipo: { [Op.like]: `%${id_tipo}%` } } : undefined,
+            permissao ? { permissao: { [Op.like]: `%${permissao}%` } } : undefined,
+        ].filter(Boolean)
+    }
+
+    const users = await tipos_usuarios.findAll({ where: pesquisa})
+
+    return res.send(users)
 
     } catch (err) {
         return res.status(403).send('error')
     }
+}
+
+exports.deleteTiposUsuarios = async (req, res) => {
+    try{
+        const encontrarTipo = await tipos_usuarios.findOne({ where: { id_tipo: req.params.id_tipo } })
+        await encontrarTipo.destroy();
+        return res.send('tipo de usuario deletado')
+} catch(error) {
+    return res.send('erroa', err)
+}
 }
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -8,7 +8,6 @@ const Fotos_quartosControllers = require('../controllers/fotos_quartos');
 const ReservasControllers = require('../controllers/reservas');
 const TiposUsuariosControllers = require('../controllers/tipos_usuarios');
 
-
 // função dos usuarios
 
 routes.post('/login', UsuariosControllers.login);
@@ -23,5 +22,9 @@ routes.put('/usuarios/:cpf', UsuariosControllers.updateUsuario);
 // função do tipos de usuario
 
 routes.post('/tiposusuarios', TiposUsuariosControllers.createTiposUsuarios);
+
+routes.get('/tiposusuarios', TiposUsuariosControllers.getTiposUsuarios);
+
+routes.delete('/tiposusuarios/:id_tipo', TiposUsuariosControllers.deleteTiposUsuarios);
 
 module.exports = routes
