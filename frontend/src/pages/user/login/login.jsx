c062a0785d09b6fe6c924872a378f49a2f569c36
isamanzano <isabelacmanzano05@gmail.com>
2025-02-08 14:51:07 -0300

    login pronto

--- frontend/src/pages/user/login/login.jsx
+++ frontend/src/pages/user/login/login.jsx
@@ -6,24 +6,27 @@ const Login = () => {
     return (
     <div className="login-page">
       <div className="fundo">
-        <button onClick={() => navigate("/")} className="back-button"> ← </button>
-        <h1 className="back-text">| LOGIN</h1>
+        <div className="back-container">
+          <button onClick={() => navigate("/")} className="back-button"> ← </button>
+          <h1 className="back-line">|</h1>
+          <h1 className="back-text">LOGIN</h1>
+        </div>
           <div className="container">
             <img className="logo_login" src="./src/assets/logo_login.png" alt="" />
           </div>
-          <form className="login-form">
-            <h1 className="login-texto">LOGIN</h1>
-            <div>
-              <input type="email" className="login-input" placeholder="EMAIL" />
-            </div>
-            <div>
-              <input type="password" className="login-input" placeholder="SENHA" />
-            </div>
-            <button type="submit" className="login-button">ENTRAR</button>
-            <div className="signup-link">
-                Não tem login? <a href="#">Cadastre-se</a>
-            </div>
-          </form>
+            <form className="login-form">
+              <h1 className="login-texto">LOGIN</h1>
+              <div>
+                <input type="email" className="login-input" placeholder="EMAIL" />
+              </div>
+              <div>
+                <input type="password" className="login-input" placeholder="SENHA" />
+              </div>
+              <button type="submit" className="login-button">ENTRAR</button>
+              <div className="signup-link">
+                  Não tem login? <a href="#">Cadastre-se</a>
+              </div>
+            </form>
       </div>
     </div>
     );
