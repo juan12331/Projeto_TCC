bbc5d397c4cc776b3e769bc80417409fc8d4fa5d
isamanzano <isabelacmanzano05@gmail.com>
2025-02-09 20:02:32 -0300

    iniciando acomodações

--- frontend/src/pages/user/acomodacoes/acomodacoes.jsx
+++ frontend/src/pages/user/acomodacoes/acomodacoes.jsx
@@ -5,9 +5,8 @@ const Acomodacoes = () => {
 
     return (
     <div className="acomodacoes-page">
-    {/* Colocar componente navbar */}
         <div className="container-acomodacoes">
-            <h1 className="acomodacoes-titulo">Acomodações Disponíveis</h1>
+            <h1 className="acomodacoes-titulo">ACOMODAÇÕES DISPONÍVEIS</h1>
             <img className="separador-acomodacoes" src="./src/assets/separador-acomodacoes.png" alt="" />
         </div>
     </div>
