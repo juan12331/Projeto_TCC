8ec0df9e97bbe58b0ddda27c0c1bb47efba6d62a
juan12331 <juanxadez2@gmail.com>
2025-02-13 17:16:55 -0300

    commit

--- backend/models/tipos_usuarios.js
+++ backend/models/tipos_usuarios.js
@@ -5,14 +5,10 @@ const { default_type } = require('mime')
 const tipos_usuarios = database.define('tipos_usuarios', {
     id_tipo: {
         type: Sequelize.INTEGER,
-<<<<<<< HEAD
         AllowNUll: false,
         primaryKey: true,
         autoIncrement: true
-=======
-        primaryKey: true,
-        autoIncrement: true,
->>>>>>> a047e5d7902b87da0614a548bd17934acb0971c2
+
     },
     permissao: {
         type: Sequelize.STRING,
