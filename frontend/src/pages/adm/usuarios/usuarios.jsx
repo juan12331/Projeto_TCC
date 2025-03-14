a6e733c07d708f3f2833ef96625ce09c6d6af534
isamanzano <isabelacmanzano05@gmail.com>
2025-03-14 10:45:57 -0300

    SUBINDO

--- frontend/src/pages/adm/usuarios/usuarios.jsx
+++ frontend/src/pages/adm/usuarios/usuarios.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./usuarios.css";
+import { PlusSquare } from "react-bootstrap-icons";
 import NavbarAdm from "../../../assets/components/navbarAdm";
 
 const Usuarios = () => {
@@ -17,11 +18,173 @@ const Usuarios = () => {
       </div>
 
       <div className="fundo-usuarios">
-        <div className="pesquisa-usuarios">
+        <div className="fundoPesquisa-usuarios">
+          <div className="pesquisa-usuarios">
           <input id='search' className='search-usuarios' placeholder='CPF ou NOME de usuário'></input>
-          <button type="submit" className="buttonSearch-usuarios">Buscar</button>
+          </div>
+        </div>
+        <button type="submit" className="buttonSearch-usuarios">BUSCAR</button>
+      </div>
+
+      <div className="layout-usuarios">
+        <div className="cards-usuarios">
+          <h1 className="dataReserva-usuarios">Data da Reserva</h1>
+        </div>
+        <div className="grid1-usuarios">
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+        </div>
+
+        <div className="cards-usuarios">
+          <h1 className="dataReserva-usuarios">Data da Reserva</h1>
+        </div>
+        <div className="grid2-usuarios">
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
+
+          <div className="fundoCards-usuarios">
+            <img 
+            className="cardsImg-usuarios" 
+            src="/src/assets/imgUsuarios_Adm/foto_usuarios.png" 
+            alt="" 
+            />
+            <h1 className="nomeCards-usuarios">Nome</h1>
+            <h2 className="cpfCards-usuarios">CPF</h2>
+          </div>
         </div>
       </div>
+
+      <div className="final-usuarios">
+        <div className="fundoFinal-usuarios">
+          <h1>Adicionar novo Usuário</h1>
+          {/* COLOCAR ICONE */}
+        </div>
+      </div>
+
     </div>
     );
 };
