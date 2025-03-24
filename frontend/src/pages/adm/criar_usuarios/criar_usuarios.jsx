5ad5b958ac95b0b742644d8d00b5184486057985
isamanzano <isabelacmanzano05@gmail.com>
2025-03-24 17:04:24 -0300

    subindo criação da pagina criar_usuario

---
+++ frontend/src/pages/adm/criar_usuarios/criar_usuarios.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./criar_usuarios.css";
+import NavbarAdm from "../../../assets/components/navbarAdm";
+
+const Criar_usuarios = () => {
+
+    return(
+    <div className="criarUser_page">
+        <NavbarAdm/>
+    </div>
+    );
+};
+
+export default Criar_usuarios
